7ccd44a130d5b9f640ba40c35d701145645924c4
artipavl <40296816+artipavl@users.noreply.github.com>
2022-11-19 19:35:23 +0200

    Виконання 1-2 завдання

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,48 @@
-export const App = () => {
-  return (
-    <>
- start
-    </>
-  );
+import { React, Component } from 'react';
+import  {nanoid}  from "nanoid";
+
+import { Section } from "components/Section/Section";
+import { Phonebook } from "components/Phonebook/Phonebook";
+import { Contacts } from "components/Contacts/Contacts";
+
+
+export class App extends Component {
+  state = {
+  contacts: [],
+    name: '',
+  number: ''
+  }
+  submitForm = (e) => {
+    e.preventDefault();
+    this.setState(({contacts, name,number}) => {
+      return {
+        contacts: [...contacts, {id: nanoid(),name: name, number: number}],
+        name: '',
+        number: '',
+      }
+    })
+    e.currentTarget.reset();
+  }
+
+  inputForm = (e) => {
+    const input = e.target
+    this.setState(stat => {
+      return {
+        ...stat,
+        [input.name]: input.value,
+      }
+    })
+  }
+  render() {
+    return (
+      <>
+        <Section title='Phonebook'>
+          <Phonebook submitForm={this.submitForm} inputForm={this.inputForm} />
+        </Section>
+        <Section title='Contacts'>
+          <Contacts contacts={this.state.contacts} />
+        </Section>
+      </>
+    )
+  }
 };
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,8 @@
+export const Contacts = ({contacts}) => {
+    return (
+        <ul>
+            {contacts.map(contact => <li key={contact.id}>{contact.name} {contact.number}</li>)}
+    </ul>
+       
+    )
+}
