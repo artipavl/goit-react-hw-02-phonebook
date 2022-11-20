39e9d32d974cef3bbfdcb16bcd073b304ea7e865
artipavl <40296816+artipavl@users.noreply.github.com>
2022-11-20 23:18:12 +0200

    add PropTypes

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,1 +1,7 @@
-export const Notification = ({message}) => <h3>{message}</h3>
+import PropTypes from 'prop-types';
+
+export const Notification = ({ message = ' ' }) => <h3>{message}</h3>;
+
+Notification.prototype = {
+  message: PropTypes.string,
+};
