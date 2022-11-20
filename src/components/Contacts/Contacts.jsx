859565b8c60fb73b559473ac51953790a7be028b
artipavl <40296816+artipavl@users.noreply.github.com>
2022-11-20 23:00:05 +0200

    add css

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,12 +1,14 @@
-export const Contacts = ({ contacts, filter,deleteContact }) => {
+import css from "components/Contacts/Contacts.module.css";
+
+export const Contacts = ({ contacts, filter, deleteContact }) => {
 
     return (
-        <ul>
+        <ul className={css.list}>
             {contacts.map(({name,number,id}) => {
                 const nameOwer = name.toLowerCase();
                 const numberOwer = number.toLowerCase();
                 const filterOwer = filter.toLowerCase();
-                return (nameOwer.includes(filterOwer) || numberOwer.includes(filterOwer)) && (<li key={id}>{name}: {number} <button data-id={id} type="button" onClick={deleteContact}>Delete</button></li>)
+                return (nameOwer.includes(filterOwer) || numberOwer.includes(filterOwer)) && (<li key={id} className={css.item}>{name}: {number} <button data-id={id} type="button" onClick={deleteContact} className={css.button}>Delete</button></li>)
             })}
     </ul>
        
