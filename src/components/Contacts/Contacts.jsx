959ef8d628fb38366ae83c093990b9259f669b2c
artipavl <40296816+artipavl@users.noreply.github.com>
2022-11-20 23:14:38 +0200

    add PropTypes

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,16 +1,42 @@
-import css from "components/Contacts/Contacts.module.css";
+import PropTypes from 'prop-types';
+import css from 'components/Contacts/Contacts.module.css';
 
 export const Contacts = ({ contacts, filter, deleteContact }) => {
-
-    return (
-        <ul className={css.list}>
-            {contacts.map(({name,number,id}) => {
-                const nameOwer = name.toLowerCase();
-                const numberOwer = number.toLowerCase();
-                const filterOwer = filter.toLowerCase();
-                return (nameOwer.includes(filterOwer) || numberOwer.includes(filterOwer)) && (<li key={id} className={css.item}>{name}: {number} <button data-id={id} type="button" onClick={deleteContact} className={css.button}>Delete</button></li>)
-            })}
+  return (
+    <ul className={css.list}>
+      {contacts.map(({ name, number, id }) => {
+        const nameOwer = name.toLowerCase();
+        const numberOwer = number.toLowerCase();
+        const filterOwer = filter.toLowerCase();
+        return (
+          (nameOwer.includes(filterOwer) ||
+            numberOwer.includes(filterOwer)) && (
+            <li key={id} className={css.item}>
+              {name}: {number}{' '}
+              <button
+                data-id={id}
+                type="button"
+                onClick={deleteContact}
+                className={css.button}
+              >
+                Delete
+              </button>
+            </li>
+          )
+        );
+      })}
     </ul>
-       
-    )
-}
+  );
+};
+
+Contacts.prototype = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  filter: PropTypes.string.isRequired,
+  deleteContact: PropTypes.func.isRequired,
+};
