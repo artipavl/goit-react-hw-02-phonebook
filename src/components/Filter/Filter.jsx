7b79d60b0898f58512f94d9b15269fea25b46a29
artipavl <40296816+artipavl@users.noreply.github.com>
2022-11-20 23:16:59 +0200

    use prettier

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,22 +1,22 @@
 import PropTypes from 'prop-types';
 
 export const Filter = ({ change, value }) => {
-    return (
-        <div>
-            <p>Find contacts by name</p>
-            <input 
-                value = {value}
-                    onChange={change}
-  type="text"
-  name="filter"
-  title="filter"
-  required
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <p>Find contacts by name</p>
+      <input
+        value={value}
+        onChange={change}
+        type="text"
+        name="filter"
+        title="filter"
+        required
+      />
+    </div>
+  );
+};
 
 Filter.prototype = {
-    change: PropTypes.string.isRequired,
-    value: PropTypes.func.isRequired
-}
+  change: PropTypes.string.isRequired,
+  value: PropTypes.func.isRequired,
+};
