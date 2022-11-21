9d6cfe23d76df926c945a5e5244628e14a6eb70e
artipavl <40296816+artipavl@users.noreply.github.com>
2022-11-21 23:16:39 +0200

    Update ContactItem.jsx

--- src/components/Contacts/ContactItem.jsx
+++ src/components/Contacts/ContactItem.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 import css from 'components/Contacts/ContactList.module.css';
 
-export const ContactItem = ({contact, deleteContact}) => {
+export const ContactItem = ({ contact, deleteContact }) => {
+    const { name, number, id } = contact;
     return (
         <li className={css.item}>
-            {contact.name}: {contact.number}
+            {name}: {number}
             <button
               type="button"
-              onClick={() => deleteContact(contact.id)}
+              onClick={() => deleteContact(id)}
               className={css.button}
             >
               Delete
